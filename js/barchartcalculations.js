c1b9f4b4077ce138a76310539ea08b19f952c992
Danny Turley <danny@performasports.com>
2020-12-01 17:59:11 +0000

    Update barchartcalculations.js

--- js/barchartcalculations.js
+++ js/barchartcalculations.js
@@ -792,7 +792,7 @@ function barChartCalc(){
 	let homePercentage = homeNum*100;
 	let awayPercentage = awayNum*100;
 
-	console.log(homePercentage,awayPercentage);
+	console.log(no1,no2);
 
 	if (no1 >= no2) {
 	$(this).find(".home-team-bar").css({width: homePercentage + "%" });
