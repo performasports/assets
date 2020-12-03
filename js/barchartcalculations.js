f15276930bb234e55b8c8edb60a2dd72c2877842
Danny Turley <danny@performasports.com>
2020-12-03 17:07:14 +0000

    Update barchartcalculations.js

--- js/barchartcalculations.js
+++ js/barchartcalculations.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
 
   //$(".gameperiod").after("<h6 class='data-row-sub-heading-label'>"+gamePeriod+"</h6>");
 
-  $.each(gamePeriod, function(value){
+  $.each(gamePeriod, function(index,value){
   $(".gameperiod").after("<h6 class='data-row-sub-heading-label'>"+value+"</h6>");
   });
 
