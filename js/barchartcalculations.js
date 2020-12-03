dcbcf4f27f7cf4a97067a2901d37cb915244bb1f
Danny Turley <danny@performasports.com>
2020-12-03 16:03:20 +0000

    Update barchartcalculations.js

--- js/barchartcalculations.js
+++ js/barchartcalculations.js
@@ -2,7 +2,7 @@ var gamePeriod = "1st Half";
 
 $(document).ready(function(){
 
-  $('team-stats-row-labels').append('<h6 class="data-row-sub-heading-label">'+gamePeriod+'</h6>');
+  $('div.team-stats-row-labels').append('<h6 class="data-row-sub-heading-label">'+gamePeriod+'</h6>');
 
 });
 
