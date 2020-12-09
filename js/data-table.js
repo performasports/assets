ebdd66eda65f771e9f15602d02a0e742768f982f
Danny Turley <danny@performasports.com>
2020-12-09 17:13:31 +0000

    Update data-table.js

--- js/data-table.js
+++ js/data-table.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 
   if (index > 0 && index < 9) {
   $(".kpi-category.one").find(".team-stats-secondary-heading-row").append("<h6 data-tippy-content='"+keyMetricsTooltips[index]+"' class='data-row-sub-heading-label category tippy'>"+value+"</h6>");
-  $(".kpi-category.one").find(".team-stats-data-row.1st Half").html("<a href='#' class='table-data w-inline-block'><h5 class='datapoint'>"+homeTeam1stHalf[index]+"</h5></a>");
+  $(".kpi-category.one").find(".team-stats-data-row.1st Half").append("<a href='#' class='table-data w-inline-block'><h5 class='datapoint'>"+homeTeam1stHalf[index]+"</h5></a>");
 
   } else {
   //  block of code to be executed if the condition is false
