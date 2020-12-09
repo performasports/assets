f8aa0b59b837b9d122ea0323272466513107607c
Danny Turley <danny@performasports.com>
2020-12-09 22:45:14 +0000

    Update barchartcalculations.js

--- js/barchartcalculations.js
+++ js/barchartcalculations.js
@@ -2,7 +2,13 @@ $(document).ready(function() {
 
   $(".bar-chart-team-stats-grid.accordion .event-heading-wrapper").click(function(){
 
+  if ($(this).parent().parent().next('.expandable-content-wrapper').hasClass('hide')) {
   $(this).parent().parent().next('.expandable-content-wrapper').slideDown('fast').toggleClass('hide');
+  }
+  else if (!$(this).parent().parent().next('.expandable-content-wrapper').hasClass('hide')) {
+  $(this).parent().parent().next('.expandable-content-wrapper').slideUp('slow').toggleClass('hide');
+  }
+
   $(this).children(".stat-heading").toggleClass("AccordionShow");
   $(this).children(".expandable-icon").toggleClass("AccordionShow");
   $(this).parent().toggleClass("selected");
