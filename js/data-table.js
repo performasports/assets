00acaaf98411edc8e9d422ccb979e5e7cc79ca3e
Danny Turley <danny@performasports.com>
2020-12-10 14:54:05 +0000

    Update data-table.js

--- js/data-table.js
+++ js/data-table.js
@@ -38,7 +38,7 @@ $(document).ready(function() {
   //  block of code to be executed if the condition is false
 	}
 
-  if (index === "Pos. Time") {
+  if (index === posTime) {
   $(".kpi-category.one").find(".data-row-sub-heading-label.category").addClass("col-lg");
   console.log("Add col-lg");
   }
