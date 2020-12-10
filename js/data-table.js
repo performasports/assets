b265973ea2fa132a26bcfca5957bf2ac98c2d243
Danny Turley <danny@performasports.com>
2020-12-10 14:14:36 +0000

    Update data-table.js

--- js/data-table.js
+++ js/data-table.js
@@ -29,20 +29,13 @@ $(document).ready(function() {
     var score = keyMetricsLabels.indexOf("Score");
     var posTime = keyMetricsLabels.indexOf("Pos. Time");
     var shotTime = keyMetricsLabels.indexOf("Shot Time");
-    //console.log(colLarge);
+    console.log(score);
+    console.log(posTime);
+    console.log(shotTime);
 
     if (index == score) {
     $(".kpi-category.one").find(".data-row-sub-heading-label.category").addClass("col-lg");
     }
-    if (index == posTime) {
-    $(".kpi-category.one").find(".data-row-sub-heading-label.category").addClass("col-lg");
-    }
-    if (index == shotTime) {
-    $(".kpi-category.one").find(".data-row-sub-heading-label.category").addClass("col-lg");
-    }
-    else {
-    //  block of code to be executed if the condition is false
-    }    
 
   if (index >= 0 && index < 9) {
   $(".kpi-category.one").find(".team-stats-secondary-heading-row").append("<h6 data-tippy-content='"+keyMetricsTooltips[index]+"' class='data-row-sub-heading-label category tippy'>"+value+"</h6>");
