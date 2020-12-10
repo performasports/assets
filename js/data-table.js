18798461248573cbc5c703cf4bd940be3528d9a8
Danny Turley <danny@performasports.com>
2020-12-10 14:04:28 +0000

    Update data-table.js

--- js/data-table.js
+++ js/data-table.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
   var colLarge = keyMetricsLabels.indexOf("Score");
   console.log(colLarge);
 
-  if (index == colLarge) {
+  if (index = colLarge) {
   $(".kpi-category.one").find(".data-row-sub-heading-label.category.tippy").addClass("col-lg");
   } else {
   //  block of code to be executed if the condition is false
