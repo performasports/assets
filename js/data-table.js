1bbf98b8e7d20d53e9b777f3deaaea579ae4a2d9
Danny Turley <danny@performasports.com>
2020-12-11 15:13:19 +0000

    Update data-table.js

--- js/data-table.js
+++ js/data-table.js
@@ -117,6 +117,9 @@ $(document).ready(function() {
   });
 
   $.each(homeTeam1stHalf, function(index,value){
+  var percentageContent = value.includes('%');
+  console.log(percentageContent);
+  
   if (index >= 0 && index < 10) {
   $(".kpi-category.one").find(".team-stats-data-row.1stHalf").append("<div class='table-data no-link w-inline-block "+keyMetricsLabelsID[index]+"'><h5 class='datapoint'>"+homeTeam1stHalf[index]+"</h5></div>");
   }
@@ -130,7 +133,7 @@ $(document).ready(function() {
   $(".kpi-category.three").find(".team-stats-data-row.1stHalf").append("<div class='table-data no-link w-inline-block "+keyMetricsLabelsID[index]+"'><h5 class='datapoint'>"+homeTeam1stHalf[index]+"</h5></div>");
   console.log("Blank",index,value);
   }
-  if (index >= 17 && index < 27 && (value !== "" || !value.includes('%'))) {
+  if (index >= 17 && index < 27 && (value !== "" || value !== percentageContent)) {
   $(".kpi-category.three").find(".team-stats-data-row.1stHalf").append("<a href='#' class='table-data w-inline-block "+keyMetricsLabelsID[index]+"'><h5 class='datapoint'>"+homeTeam1stHalf[index]+"</h5></a>");
   console.log(index,value);
   }
