0579b86e0095a8de74c017c650d8af191601d227
Danny Turley <danny@performasports.com>
2020-12-11 13:38:46 +0000

    Update data-table.js

--- js/data-table.js
+++ js/data-table.js
@@ -126,7 +126,7 @@ $(document).ready(function() {
   if (index >= 10 && index < 17 && value !== "") {
   $(".kpi-category.two").find(".team-stats-data-row.1stHalf").append("<a href='#' class='table-data w-inline-block "+keyMetricsLabelsID[index]+"'><h5 class='datapoint'>"+homeTeam1stHalf[index]+"</h5></a>");
   }
-  if (index >= 17 && index < 27 && value == "") {
+  if (index >= 17 && index < 27 && (value == "" || keyMetricsLabelsID[index] == "-")) {
   $(".kpi-category.three").find(".team-stats-data-row.1stHalf").append("<div class='table-data no-link w-inline-block "+keyMetricsLabelsID[index]+"'><h5 class='datapoint'>"+homeTeam1stHalf[index]+"</h5></div>");
   }
   if (index >= 17 && index < 27 && value !== "") {
