c9b99b3a5ab7bfd5dc53930033fe463b762fdcac
Danny Turley <danny@performasports.com>
2020-12-14 16:53:28 +0000

    Update data-table.js

--- js/data-table.js
+++ js/data-table.js
@@ -2392,7 +2392,7 @@ $(document).ready(function() {
     }
     else if (!$(this).hasClass('selected')) {
       $(".data-filters-links.show-all").addClass("selected");
-      $(".data-filters-links").addClass("selected");
+      $(".data-filters-links").removeClass("selected");
     }
   });
 
