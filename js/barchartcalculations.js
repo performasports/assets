686114f767e694840d20e240c98b42cf792fc2d6
Danny Turley <danny@performasports.com>
2020-12-15 11:35:50 +0000

    Update barchartcalculations.js

--- js/barchartcalculations.js
+++ js/barchartcalculations.js
@@ -117,7 +117,7 @@ function barChartCalc(){
 function barChartExpPts(){
 
 	$('.team-stats').each(function() {
-	let no1 = $(this).find("#home-team-exppts-number").toFixed(2);;
+	let no1 = $(this).find("#home-team-exppts-number").val();
 	let no2 = parseInt($(this).find("#away-team-exppts-number").text(),10);
 	let total = no1+no2; //10
 
