2159d4bce28f7eb20b8cf4963f9a19aa88bc7274
Danny Turley <danny@performasports.com>
2021-03-18 21:58:33 +0000

    Create boycott-ie.js

---
+++ js/boycott-ie.js
@@ -0,0 +1,13 @@
+if (/*@cc_on!@*/false || (!!window.MSInputMethodContext && !!document.documentMode)){
+var lang = navigator.language||navigator.userLanguage;
+langtwo = lang.substr(0, 2);
+
+//Chinese
+if(langtwo == 'zh'){ if(lang == 'zh-Hant' || lang == 'zh-TW' || lang == 'zh-tw'){window.location.href="https://www.performasports.tv/update-your-browser";}
+ else{window.location.href="https://www.performasports.tv/update-your-browser";}
+                   }
+//else
+
+else if(lang == 'haw'){window.location.href="https://www.performasports.tv/update-your-browser";}
+//else English
+ else{window.location.href="https://www.performasports.tv/update-your-browser";}}
