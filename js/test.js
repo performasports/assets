1eb3dfcb3fd5eaf0c78c6d4135734bea92d00af3
Danny Turley <danny@performasports.com>
2022-07-14 22:18:32 +0100

    Update test.js

--- js/test.js
+++ js/test.js
@@ -44,12 +44,40 @@ function Add(){
     else {
     }
 
+    function shotsPercent(){
+     let no1 = parseInt($("#home-shotspercent-number").text(),10);
+     let no2 = parseInt($("#away-shotspercent-number").text(),10);
+     let total = no1+no2; //10
+     let homeNum = no1/total;
+     let awayNum = no2/total;
+     let homePercentage = homeNum*100;
+     let awayPercentage = awayNum*100;
+     //alert("The result is "+result +no1 +no2);
+
+     //$("#home-psr-bar").css({width: homePercentage + "%" });
+
+     if (no1 >= no2) {
+     $("#home-shotspercent-bar").css({width: homePercentage + "%" });
+     $("#away-shotspercent-bar").css({width: 100 + "%" });
+ }
+     if (no1 == 0 && no2 == 0) {
+     $("#home-shotspercent-bar").css({width: 50 + "%" });
+     $("#away-shotspercent-bar").css({width: 100 + "%" });
+     }
+     if (no2 > no1) {
+     $("#home-shotspercent-bar").css({width: 100 + "%" });
+     $("#away-shotspercent-bar").css({width: awayPercentage + "%" }).css("z-index","2");
+ }
+     else {
+     }
+
    }
 
 
    //cdn//
    function initialise() {
    shots();
+   shotsPercent();
    }
 
 $(document).ready(initialise);
